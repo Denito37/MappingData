f7ecd6ccb4c4199f29b0ac681224adcf89344561
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-10-28 14:06:45 -0400

    implemented part of transformation logic

--- etl/extract/extract.py
+++ etl/extract/extract.py
@@ -1,8 +1,14 @@
 import requests
 import pandas as pd
+from dotenv import load_dotenv
+import os
 
-drinkingFountainsAPIURL = "https://data.cityofnewyork.us/api/v3/views/qnv7-p7a2/query.json"
-treesCensusAPIURL = "https://data.cityofnewyork.us/api/v3/views/uvpi-gqnh/query.json"
+load_dotenv()
+API_TOKEN = os.getenv("API_TOKEN")
+limit = 1000
+pageNum = 1
+drinkingFountainsAPIURL = f"https://data.cityofnewyork.us/api/v3/views/qnv7-p7a2/query.json?page_number={pageNum}&page_size={limit}&app_token={API_TOKEN}"
+treesCensusAPIURL = f"https://data.cityofnewyork.us/api/v3/views/uvpi-gqnh/query.json?page_number={pageNum}&page_size={limit}&app_token={API_TOKEN}"
 
 def fetch_data(url):
     response = requests.get(url)
--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -2,7 +2,7 @@ import pandas as pd
 from etl.extract import extract_drinking_fountains_data, extract_trees_census_data
 
 dropFountainColumns = [
-    'painted', 'fountain_count', 'fountain_type', 'gispropnum','district', 'ompropid'
+    'painted', 'fountainco', 'fountainty', 'gispropnum','decription', 'parentid'
 ]
 
 dropTreeColumns = [
@@ -10,6 +10,28 @@ dropTreeColumns = [
     'trunk_wire', 'trunk_other', 'trunk_light', 'brch_light', 'brch_heavy', 'brch_other',
     'sidewalk', 'zip_city', 'state', 'cb_num', 'borocode',
     'x_sp', 'y_sp', 'block_id', 'bbl', 'bin', 'spc_latin', 'steward',
-    'guards', 'sidewalk', 'health','tree_dhb','user_type','problems', 'address', 'postcode', 'cncldist',
-    'st_assem', 'st_senate', 'nta', 'nta_name', 'boro_ct'
+    'guards', 'sidewalk', 'health','tree_dhb','user_type','problems', 'address', 'zipcode', 'cncldist',
+    'st_assem', 'st_senate', 'nta', 'nta_name', 'boro_ct', 'census_tract', 'council_district'
 ]
+
+def transform_drinking_fountains_data():
+    data = extract_drinking_fountains_data()
+    fountains = pd.json_normalize(data)
+    fountains_cleaned = fountains.drop(columns=dropFountainColumns)
+    fountains_cleaned = fountains_cleaned.rename(columns={
+        'system': 'id',
+        'featurestatus': 'status',
+        'propertyna':'located_at'
+    })
+    return fountains_cleaned
+
+def transform_trees_census_data():
+    data = extract_trees_census_data()
+    trees = pd.json_normalize(data)
+    trees_cleaned = trees.drop(columns=dropTreeColumns)
+    trees_cleaned = trees_cleaned.rename(columns={
+        
+    })
+    return trees_cleaned
+
+# run transformation in batches   
