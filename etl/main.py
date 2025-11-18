7083a45b7241221152d6b8feb4e1d8f34988f94a
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-11-17 21:10:51 -0500

    implemented function to test etl process

--- etl/main.py
+++ etl/main.py
@@ -3,44 +3,39 @@ from transform.transform import transform_drinking_fountains_data, transform_tre
 from load.load import load_data
 import time
 import asyncio
-import pandas as pd
+import folium as fm
 
-async def test_ETL():
-    testData = [
-        {
-            ':updated_at':'test',
-            'id':'123',
-            'located_at':'test',
-            'borough':'test',
-            'department':'test',
-            'status':'test',
-            'coordinates':'test'
-
-    },
-    {
-            ':updated_at':'test',
-            'id':'456',
-            'located_at':'test',
-            'borough':'test',
-            'department':'test',
-            'status':'test',
-            'coordinates':'test'
-    },
-    {
-            ':updated_at':'test',
-            'id':'789',
-            'located_at':'test',
-            'borough':'test',
-            'department':'test',
-            'status':'test',
-            'coordinates':'test'
-
-    }]
-    testData = pd.DataFrame(testData)
+def test_Map():
     fountain = read_drinking_fountains_data()
     fountainTransformed = transform_drinking_fountains_data(fountain)
-    print(fountainTransformed.info())
-    await load_data('Fountains', testData)
+
+    map = fm.Map(
+        max_bounds=True,
+        location=[40.7,-74.0],
+        min_lat= 40.49,
+        max_lat=40.92,
+        min_lon=-74.27,
+        max_lon=-73.97
+    )
+
+    fm.TileLayer('OpenStreetMap', overlay=True).add_to(map)
+
+    for index, row in fountainTransformed.iterrows():
+        fm.Marker(
+            location=[row['coordinates'].x,row['coordinates'].y],
+            popup=row['located_at'],
+            icon=fm.Icon(color='blue'),
+            lazy=True
+        ).add_to(map)
+
+    map.save('index.html')
+    return
+async def test_ETL(): 
+    fountain = read_drinking_fountains_data()
+    fountainTransformed = transform_drinking_fountains_data(fountain)
+    for index, row in fountainTransformed.iterrows():
+        fountainTransformed['coordinates'] = f'{row['coordinates'].x},{row['coordinates'].y}'
+    await load_data('Fountains', fountainTransformed)
     return 0
 
 def main():
@@ -48,6 +43,7 @@ def main():
     start = time.time()
 
     asyncio.run(test_ETL())
+    #test_Map()
 
     end = time.time()
     print(f"Execution time: {round(end - start)} Second(s)")
