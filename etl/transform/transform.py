8ed551e0d178f95dfe60424be2eaeb856f9df6c1
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-11-21 15:42:19 -0500

    updated transformation logic to be store data more effciently

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -13,6 +13,15 @@ renameFountainColumns = {
     'the_geom':'coordinates'
 }
 
+typeCastFountainColumns = {
+    ':updated_at':'string',
+    'id': 'string',
+    'status':'category',
+    'located_at':'string',
+    'borough':'category',
+    'department':'string'
+}
+
 dropTreeColumns = [
     'stump_diam', 'curb_loc', 'root_stone', 'root_grate', 'root_other',
     'trunk_wire', 'trnk_other', 'trnk_light', 'brch_light', 'brch_shoe', 'brch_other',
@@ -20,7 +29,7 @@ dropTreeColumns = [
     'x_sp', 'y_sp', 'block_id', 'bbl', 'bin', 'spc_latin', 'steward',
     'guards', 'sidewalk', 'health','tree_dbh','user_type','problems', 'address', 'zipcode', 'cncldist',
     'st_assem', 'st_senate', 'nta', 'nta_name', 'boro_ct', 'census_tract', 'council_district',
-    ':id',':version',':created_at','created_at','latitude','longitude'
+    ':id',':version',':created_at','created_at'
 ]
 
 renameTreeColumns = {
@@ -30,17 +39,29 @@ renameTreeColumns = {
     'cb_num': 'district'
 }
 
+typeCastTreeColumns = {
+    ':updated_at':'string',
+    'id': 'string',
+    'status':'category',
+    'name':'string',
+    'district':'string',
+    'borough':'category'
+}
+
 def transform_drinking_fountains_data(data):
     fountainsCleaned = data.drop(dropFountainColumns, axis=1)
     fountainsCleaned = fountainsCleaned.rename(columns=renameFountainColumns)
+    fountainsCleaned = fountainsCleaned.astype(typeCastFountainColumns)
     fountainsCleaned['coordinates'] = fountainsCleaned['the_geom.coordinates'].apply(lambda coords: Point(coords) if coords else None)
     fountainsCleaned = gpd.GeoDataFrame(fountainsCleaned, geometry='coordinates')
     fountainsCleaned = fountainsCleaned.drop(columns=['the_geom.coordinates', 'the_geom.type'])
     return fountainsCleaned
 
 def transform_trees_census_data(data):
-    data = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data.longitude, data.latitude))
     treesCleaned = data.drop(dropTreeColumns,axis=1)
+    treesCleaned = gpd.GeoDataFrame(treesCleaned, geometry=gpd.points_from_xy(treesCleaned.longitude, treesCleaned.latitude))
+    treesCleaned = treesCleaned.drop(columns=['latitude','longitude'])
     treesCleaned = treesCleaned.rename(columns=renameTreeColumns)
+    treesCleaned = treesCleaned.astype(typeCastTreeColumns)
     treesCleaned['name'] = treesCleaned['name'].fillna('Unknown')
     return treesCleaned  
